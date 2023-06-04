d6a2073c12b21f4ceb537c2e923a7e940f745e23
rizwanhossainrahat <rizwanhossainrahat400@gmail.com>
2023-06-04 12:11:42 +0600

    static memory,dynamic memory,ternary operator

---
+++ dynamic_array_increase.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int *a=new int[5];
+    int *b=new int[7];
+    for (int i = 0; i < 5; i++)
+    {
+        cin>>a[i];
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        b[i]=a[i];
+    }
+    b[5]=40;
+    b[6]=50;
+    for (int i = 0; i < 7; i++)
+    {
+       cout<<b[i]<<" ";
+    }
+    cout<<endl;
+    // delete a array
+    delete[] a;
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    
+    
+    
+    return 0;
+}
---
+++ terbnary_operator.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x=11;
+    // if(x%2==0)
+    // {
+    //     cout<<"Even";
+    // }
+    // else
+    // {
+    //     cout<<"Odd";
+    // }
+
+    //ternary operator
+    (x%2==0) ? cout<<"Even" : cout<<"Odd";
+    return 0;
+}
